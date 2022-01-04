fc6f13dd929ec53c3080e8937a31163a1808f6e7
Luca Casonato <hello@lcas.dev>
2022-01-04 14:33:04 +0100

    add more http examples (#7)

---
+++ data/http-server-streaming.ts
@@ -0,0 +1,44 @@
+/**
+ * @title HTTP Server: Streaming
+ * @difficulty intermediate
+ * @tags cli, deploy
+ * @run --allow-net <url>
+ *
+ * An example HTTP server that streams a response back to the client.
+ */
+
+// Import the http server from std/http.
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+
+function handler(_req: Request): Response {
+  // Set up a variable to store a timer ID, and the ReadableStream.
+  let timer: number | undefined = undefined;
+  const body = new ReadableStream({
+    // When the stream is first created, start an interval that will emit a
+    // chunk every second containing the current time.
+    start(controller) {
+      timer = setInterval(() => {
+        const message = `It is ${new Date().toISOString()}\n`;
+        controller.enqueue(new TextEncoder().encode(message));
+      }, 1000);
+    },
+    // If the stream is closed (the client disconnects), cancel the interval.
+    cancel() {
+      if (timer !== undefined) {
+        clearInterval(timer);
+      }
+    },
+  });
+
+  // Return a response with the stream as the body.
+  return new Response(body, {
+    headers: {
+      "content-type": "text/plain",
+      "x-content-type-options": "nosniff",
+    },
+  });
+}
+
+// To start the server on the default port, call `serve` with the handler.
+console.log("Listening on http://localhost:8000");
+serve(handler);
