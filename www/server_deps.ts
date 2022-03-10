a710e0a91c079a261b615c18622229b2b1a3194c
Luca Casonato <hello@lcas.dev>
2022-03-11 00:38:02 +0100

    fix highlighting

--- www/server_deps.ts
+++ www/server_deps.ts
@@ -4,10 +4,11 @@ export * from "https://raw.githubusercontent.com/lucacasonato/fresh/67b3eb470e35
 export * as gfm from "https://deno.land/x/gfm@0.1.19/mod.ts";
 
 // npm:prismjs
-export { default as Prism } from "https://esm.sh/prismjs@1.26.0";
-import "https://esm.sh/prismjs@1.26.0/components/prism-jsx.js?no-check";
-import "https://esm.sh/prismjs@1.26.0/components/prism-typescript.js?no-check";
-import "https://esm.sh/prismjs@1.26.0/components/prism-tsx.js?no-check";
+import { default as Prism } from "https://esm.sh/prismjs@1.27.0";
+import "https://esm.sh/prismjs@1.27.0/components/prism-jsx.js?no-check";
+import "https://esm.sh/prismjs@1.27.0/components/prism-typescript.js?no-check";
+import "https://esm.sh/prismjs@1.27.0/components/prism-tsx.js?no-check";
+export { Prism };
 
 // npm:twind
 export { virtualSheet } from "https://esm.sh/twind@0.16.16/sheets?pin=v57";
