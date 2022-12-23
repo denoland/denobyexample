c7cd93bdfb72d575f1df091c1010a14740fb13b2
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-12-23 09:09:59 -0800

    fmt

--- _tools/check_valid_data.ts
+++ _tools/check_valid_data.ts
@@ -1,16 +1,16 @@
-import { TOC } from '../toc.js'
+import { TOC } from "../toc.js";
 
-for await (const dirEntry of Deno.readDir('data')) {
-  if(dirEntry.isDirectory) {
-    throw `Unexpected directory ${dirEntry.name} in data`
+for await (const dirEntry of Deno.readDir("data")) {
+  if (dirEntry.isDirectory) {
+    throw `Unexpected directory ${dirEntry.name} in data`;
   }
-  if(dirEntry.isSymlink) {
-    throw `Unexpected symlink ${dirEntry.name} in data`
+  if (dirEntry.isSymlink) {
+    throw `Unexpected symlink ${dirEntry.name} in data`;
   }
 
-  const slug = dirEntry.name.slice(0, -3)
+  const slug = dirEntry.name.slice(0, -3);
 
-  if(!TOC.includes(slug)) {
-    throw `${dirEntry.name} is not listed in toc.js`
+  if (!TOC.includes(slug)) {
+    throw `${dirEntry.name} is not listed in toc.js`;
   }
 }
