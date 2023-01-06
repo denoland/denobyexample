b54d646eb6e87c1848ac55e3f5e2285c9cfad8bf
Mike Mulchrone <mtmulch0191@outlook.com>
2023-01-05 20:24:51 -0500

    tcp listener

---
+++ data/tcp-listener.ts
@@ -0,0 +1,21 @@
+/**
+ * @title TCP Listener: Ping
+ * @difficulty beginner
+ * @tags cli
+ * @run --allow-net <url>
+ *
+ * An example of a TCP listener on localhost that will log the message if written to and close the connection if connected to.
+ */
+const decoder = new TextDecoder();
+
+const listener = Deno.listen({
+  hostname: "127.0.0.1",
+  port: 8080,
+  transport: "tcp",
+});
+for await (const conn of listener) {
+  const buf = new Uint8Array(1024);
+  await conn.read(buf);
+  console.log("Server - received: ", decoder.decode(buf));
+  conn.close();
+}
