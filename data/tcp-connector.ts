8dd2f9cb95e6bcaa9583b5c7461d6b571db31a38
Mike Mulchrone <mtmulch0191@outlook.com>
2023-01-18 18:54:12 -0500

    adjustments

--- data/tcp-connector.ts
+++ data/tcp-connector.ts
@@ -3,8 +3,8 @@
  * @difficulty intermediate
  * @tags cli
  * @run --allow-net <url>
- * @resource {https://deno.land/api?s=Deno.listen} Deno.listen: API
- * @resource {https://github.com/denoland/denobyexample/blob/main/data/tcp-listener.ts} Example TCP server that will accept connections
+ * @resource {https://deno.land/api?s=Deno.connect} Deno.connect: API
+ * @resource {/tcp-listener.ts} Example TCP server that will accept connections
  *
  * An example of connecting to a TCP server on localhost and writing a 'ping' message to the server.
  */
