85b9ffab3dc40ea0397d58b6eaa2432a9435c3db
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-01-21 09:32:12 -0800

    fix lint errors

--- data/dns-queries.ts
+++ data/dns-queries.ts
@@ -14,14 +14,17 @@
 // In the most basic basic case, we can query a domain's A record.
 // This will give us a list of ipv4 addresses.
 const a = await Deno.resolveDns("example.com", "A");
+console.log(a)
 
 // We can also query other record types. In this case we are querying
 // an MX record which is related to email protocols. Deno supports querying
 // A, AAAA, ANAME, CAA, CNAME, MX, NAPTR, NS, PTR, SOA, SRV, and TXT records.
 const mx = await Deno.resolveDns("example.com", "MX");
+console.log(mx)
 
 // We are also optionally able to specify a nameserver via an ip address
 // and (optionally) a port number. To override the system configuration.
 const aaaa = await Deno.resolveDns("example.com", "AAAA", {
   nameServer: { ipAddr: "8.8.8.8", port: 53 },
 });
+console.log(aaaa)
