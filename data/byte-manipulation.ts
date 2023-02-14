bb50285e448a92866b28c08be411e5270ccb989e
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-02-13 20:16:04 -0800

    feat: byte manipulation

---
+++ data/byte-manipulation.ts
@@ -0,0 +1,33 @@
+/**
+ * @title Manipulating byte arrays
+ * @difficulty beginner
+ * @tags cli
+ * @run <url>
+ * @resource {$std/bytes} Doc: std/bytes
+ *
+ * When working with lower-level data we often deal
+ * with byte arrays in the form of Uint8Arrays. There
+ * are some common manipulations and queries that can
+ * be done and are included with the standard library.
+ */
+
+// Let's initialize some byte arrays
+const a = new Uint8Array([0, 1, 2, 3, 4]);
+const b = new Uint8Array([5, 6, 7, 8, 9]);
+const c = new Uint8Array([4, 5]);
+
+// We can concatenate two byte arrays using the
+// concat method
+import { concat } from "$std/bytes/concat.ts";
+const d = concat(a, b);
+console.log(d); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// Sometimes we need to repeat certain bytes
+import { repeat } from "$std/bytes/repeat.ts";
+console.log(repeat(c, 4)); // [4, 5, 4, 5, 4, 5, 4, 5]
+
+// Sometimes we need to mutate a Uint8Array and need a copy
+import { copy } from "$std/bytes/copy.ts";
+const cpy = new Uint8Array(5);
+console.log("Bytes copied:", copy(b, cpy)); // 5
+console.log("Bytes:", cpy); // [5, 6, 7, 8, 9]
