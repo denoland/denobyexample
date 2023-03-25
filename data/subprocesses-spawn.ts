e9fc3de085160232e6ac12f6a87bf02093a44882
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-03-25 12:24:18 -0700

    feat: subprocess spawning

---
+++ data/subprocesses-spawn.ts
@@ -0,0 +1,40 @@
+/**
+ * @title Subprocesses: Spawning
+ * @difficulty intermediate
+ * @tags cli
+ * @run --allow-run <url>
+ * @resource {https://deno.land/api?s=Deno.Command} Doc: Deno.Command
+ *
+ * For more complex usecases, we don't simply want the output of some
+ * command. In this case, we can spawn a subprocess and interact with
+ * it.
+ */
+
+// The Deno namespace has a unified api for interacting with the outside system
+// called Deno.Command. With it, we can initialize some information about the
+// command but it will not be executed immediately.
+const command = new Deno.Command("deno", {
+  args: [
+    "fmt",
+    "-",
+  ],
+  stdin: "piped",
+  stdout: "piped",
+});
+
+// In the a slightly more complex case, we want to interact with a spawned
+// process. To do this, we first need to spawn it.
+const process = await command.spawn();
+
+// We can now pipe the input into stdin. To do this we must first get
+// a writer from the stream and write to it
+const writer = await process.stdin.getWriter();
+writer.write(new TextEncoder().encode("console.log('hello')"));
+writer.releaseLock();
+
+// We must then close stdin
+await process.stdin.close();
+
+// We can now wait for the process to output the results
+const result = await process.output();
+console.log(new TextDecoder().decode(result.stdout));
