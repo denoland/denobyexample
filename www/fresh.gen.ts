776c8343056173099e1de9187c2d1a12d615733e
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-06-24 12:35:01 -0700

    chore: bump fresh and std versions

--- www/fresh.gen.ts
+++ www/fresh.gen.ts
@@ -2,7 +2,6 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[id]/[file].ts";
 import * as $1 from "./routes/[id]/index.tsx";
 import * as $2 from "./routes/gfm.css.ts";
@@ -20,7 +19,6 @@ const manifest = {
     "./islands/CopyButton.tsx": $$0,
   },
   baseUrl: import.meta.url,
-  config,
 };
 
 export default manifest;
