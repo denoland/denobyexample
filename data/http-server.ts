5dbf88f07d52e7836233e2da54d677d418ccffb6
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-07-12 19:24:58 -0700

    chore: update comment

--- data/http-server.ts
+++ data/http-server.ts
@@ -17,5 +17,5 @@ function handler(_req: Request): Response {
   return new Response("Hello, World!");
 }
 
-// To start the server on the default port, call `serve` with the handler.
+// To start the server on the default port, call `Deno.serve` with the handler.
 Deno.serve(handler);
