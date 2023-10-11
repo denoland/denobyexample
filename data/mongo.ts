04c82d406531c0e686b9961b658ea19f3f3864b6
Shajan Jacob <shajanjp@gmail.com>
2023-10-11 08:41:19 +0530

    fix: updates the example with npm mongodb package

--- data/mongo.ts
+++ data/mongo.ts
@@ -2,33 +2,29 @@
  * @title Connect to MongoDB
  * @difficulty intermediate
  * @tags cli, deploy
- * @run --allow-net --allow-env <url>
+ * @run --allow-net --allow-sys --allow-read <url>
  * @resource {https://deno.land/x/mongo} Deno MongoDB on deno.land/x
  *
  * Using the Deno MongoDB client, you can connect to a Mongo database
  * running anywhere.
  */
 
-import {
-  MongoClient,
-  ObjectId,
-} from "https://deno.land/x/mongo@v0.32.0/mod.ts";
+import { MongoClient } from "npm:mongodb@6.1.0";
 
-// Create a new instance of the MongoDB client
-const client = new MongoClient();
+// Create a new instance of the MongoDB client running locally on port 27017
+const client = new MongoClient("mongodb://127.0.0.1:27017");
 
-// Connect to the MongoDB server running locally on port 27017
-await client.connect("mongodb://127.0.0.1:27017");
+// Connect to the MongoDB server
+await client.connect();
 
 // Define the schema for the collection
 interface DinosaurSchema {
-  _id: ObjectId; // MongoDB ObjectID, a unique identifier
   name: string;
   skills: string[];
 }
 
 // Access the database
-const db = client.database("animals");
+const db = client.db("animals");
 
 // Access the collection within the database
 const dinosaurs = db.collection<DinosaurSchema>("dinosaurs");
