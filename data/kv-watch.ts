b2391e37afcaa087616eecfbbbae1e55ac4bc90c
Andy Jiang <andyjiang@Andys-MacBook-Pro.local>
2023-12-12 15:44:09 -0800

    update example

--- data/kv-watch.ts
+++ data/kv-watch.ts
@@ -13,15 +13,31 @@
 // Open the default database
 const kv = await Deno.openKv();
 
-// Set value in ["counter"] to 0, then increment every second.
+// Set "counter" value to 0, then increment every second.
 await kv.set(["counter"], new Deno.KvU64(0n));
 setInterval(() => {
   kv.atomic().sum(["counter"], 1n).commit();
 }, 1000);
 
-// Create a server that responds with a stream. Each time
-// a change to ["counter"] is detected, send the updated value to
-// the client.
+// Listen for changes to "counter" and log value.
+for await (const [entry] of kv.watch([["counter"]])) {
+  console.log(`Counter: ${entry.value}`);
+}
+
+// You can also create a stream reader from kv.watch, which returns
+// a ReadableStream.
+const stream = kv.watch([["counter"]]).getReader();
+while (true) {
+  const counter = await stream.read();
+  if (counter.done) {
+    break;
+  }
+  console.log(`Counter: ${counter.value[0].value}`);
+}
+
+// To use server-sent events, let's create a server that
+// responds with a stream. Each time a change to "counter"
+// is detected, send the updated value to the client.
 Deno.serve((_req) => {
   const stream = kv.watch([["counter"]]).getReader();
   const body = new ReadableStream({
