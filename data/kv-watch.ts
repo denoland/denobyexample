41f4023b0ed0ef372aca578299d101738d73f4ee
Andy Jiang <andyjiang@gmail.com>
2023-12-13 10:05:48 -0800

    add kv watch example (#115)
    * add kv watch example
    
    * update example
    
    ---------
    
    Co-authored-by: Andy Jiang <andyjiang@Andys-MacBook-Pro.local>

---
+++ data/kv-watch.ts
@@ -0,0 +1,64 @@
+/**
+ * @title Deno KV Watch
+ * @difficulty intermediate
+ * @tags cli, deploy
+ * @run --unstable <url>
+ * @resource {https://deno.land/manual/runtime/kv} Deno KV user guide
+ * @resource {https://deno.land/api?unstable=&s=Deno.Kv} Deno KV Runtime API docs
+ *
+ * Deno KV watch allows you to detect changes to your KV database, making
+ * it easier to build real-time applications, newsfeeds, chat, and more.
+ */
+
+// Open the default database
+const kv = await Deno.openKv();
+
+// Set "counter" value to 0, then increment every second.
+await kv.set(["counter"], new Deno.KvU64(0n));
+setInterval(() => {
+  kv.atomic().sum(["counter"], 1n).commit();
+}, 1000);
+
+// Listen for changes to "counter" and log value.
+for await (const [entry] of kv.watch([["counter"]])) {
+  console.log(`Counter: ${entry.value}`);
+}
+
+// You can also create a stream reader from kv.watch, which returns
+// a ReadableStream.
+const stream = kv.watch([["counter"]]).getReader();
+while (true) {
+  const counter = await stream.read();
+  if (counter.done) {
+    break;
+  }
+  console.log(`Counter: ${counter.value[0].value}`);
+}
+
+// To use server-sent events, let's create a server that
+// responds with a stream. Each time a change to "counter"
+// is detected, send the updated value to the client.
+Deno.serve((_req) => {
+  const stream = kv.watch([["counter"]]).getReader();
+  const body = new ReadableStream({
+    async start(controller) {
+      while (true) {
+        if ((await stream.read()).done) {
+          return;
+        }
+        const data = await kv.get(["counter"]);
+        controller.enqueue(
+          new TextEncoder().encode(`Counter: ${data.value}\n`),
+        );
+      }
+    },
+    cancel() {
+      stream.cancel();
+    },
+  });
+  return new Response(body, {
+    headers: {
+      "Content-Type": "text/event-stream",
+    },
+  });
+});
