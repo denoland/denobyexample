dd65692bf6d38b99d9723259ddebdbcaeddf0ce5
kLiHz <66966137+kLiHz@users.noreply.github.com>
2024-05-01 17:33:06 +0800

    Bump to Twind v1
    https://fresh.deno.dev/docs/examples/using-twind-v1

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,10 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { defineConfig, Preset } from "https://esm.sh/@twind/core@1.1.3";
+import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
+import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
 
 export default {
+  ...defineConfig({
+    presets: [presetTailwind() as Preset, presetAutoprefix()],
+  }),
   selfURL: import.meta.url,
-} as Options;
+};
